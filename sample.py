20152bf8a2b7d274731a227ccc346f9a2ab5b397
David Allemang <allemang.d@gmail.com>
2019-04-28 16:34:43 -0400

    use duodecaplex edge cosets example

--- sample.py
+++ sample.py
@@ -3,9 +3,9 @@ from toddcox import solve
 
 
 def main():
-    # gens, subgens, rels = schlafli('rgby', 'rg', (5, 3, 3))
+    gens, subgens, rels = schlafli('rgby', 'rg', (5, 3, 3))
 
-    print(solve('ab', '', ('a' * 2, 'b' * 2, 'ab' * 3)))
+    print(solve(gens, subgens, rels))
 
 
 if __name__ == '__main__':
